8dea053c51a79ab8e7554a789af6fedb39b43b58
Keshav007-01 <keshav.mohta7@gmail.com>
2026-03-21 01:33:18 +0530

    Add android-project-capacitor-fixed/capacitor-android/src/main/java/com/getcapacitor/BridgeActivity.java

---
+++ android-project-capacitor-fixed/capacitor-android/src/main/java/com/getcapacitor/BridgeActivity.java
@@ -0,0 +1,26 @@
+package com.getcapacitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BridgeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+    
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
